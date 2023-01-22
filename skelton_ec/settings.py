44d08d5caec3d05a3714166a42b8bb4f9fbfc7c6
korombus <korombus@users.noreply.github.com>
2023-01-22 14:13:37 +0900

    staticでcssが読み込めない問題を修正

--- skelton_ec/settings.py
+++ skelton_ec/settings.py
@@ -124,9 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+PROJECT_ROOT = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+DJANGO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = (
+    [os.path.join(DJANGO_ROOT, 'static')]
+)
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
